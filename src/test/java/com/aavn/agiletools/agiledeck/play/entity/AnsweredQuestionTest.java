82bfd4b4deec5b63cdb5f22dfa18d19af2ac4e45
LNHTai <tai.le01@axonactive.com>
2020-11-09 10:21:53 +0700

    Config jenkins file

--- src/test/java/com/aavn/agiletools/agiledeck/play/entity/AnsweredQuestionTest.java
+++ src/test/java/com/aavn/agiletools/agiledeck/play/entity/AnsweredQuestionTest.java
@@ -3,6 +3,7 @@ package com.aavn.agiletools.agiledeck.play.entity;
 import com.aavn.agiletools.agiledeck.game.entity.Answer;
 import com.aavn.agiletools.agiledeck.game.entity.Game;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,16 +11,26 @@ import java.util.List;
 
 class AnsweredQuestionTest {
 
+    private List<Answer> answers;
+    private GameBoard gameBoard;
+
+    @BeforeEach
+    public void init() {
+        answers = Arrays.asList(
+                new Answer(1L),
+                new Answer(2L)
+        );
+
+        Game game = new Game();
+        game.setName("Planning Poker");
+
+        gameBoard = new GameBoard("dfe24c11-e3fe-45db-9a11-033cd6a51fec", game);
+    }
+
     @Test
     public void whenCreateWithoutQuestion_thenReturnCorrectAnswer() {
-        List<Answer> answerList = Arrays.asList(
-                new Answer(1L),
-                new Answer(2L),
-                new Answer(3L),
-                new Answer(4L)
-        );
-        Game game = new Game();
-        GameBoard gameBoard = new GameBoard("sdf-asdfag-sdvdafgfda-ger", game);
-        Assertions.assertNotNull(AnsweredQuestion.createWithoutQuestion(gameBoard, answerList));
+        AnsweredQuestion answeredQuestion = AnsweredQuestion.createWithoutQuestion(gameBoard, answers);
+
+        Assertions.assertNotNull(answeredQuestion);
     }
 }
