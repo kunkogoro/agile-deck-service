43f7ff258214a4cfd6e76d8bad56e43b67416124
LNHTai <tai.le01@axonactive.com>
2020-11-19 16:17:20 +0700

    Change migration file for test

--- src/main/resources/db/migration/V1.0.0__init_data.sql
+++ src/main/resources/db/migration/V1.0.0__init_data.sql
@@ -1,52 +1,52 @@
 -- table tbl_games;
-INSERT INTO tbl_games (id, name, description) 
-VALUES (1, 'Iterative - Incremental - Big Bang', 'A workshop game to encourage people to think about alternative approaches for tackling projects. - by Scum & Kanban');
+INSERT INTO tbl_games (name, description) 
+VALUES ('Iterative - Incremental - Big Bang', 'A workshop game to encourage people to think about alternative approaches for tackling projects. - by Scum & Kanban');
 
 -- table tbl_answer_groups;
-INSERT INTO tbl_answer_groups (id, name)
-VALUES (1, 'Approach set');
+INSERT INTO tbl_answer_groups (name)
+VALUES ('Approach set');
 
 -- table tbl_answers;
-INSERT INTO tbl_answers (id, answer_content, number_order, answer_content_as_image, answer_group_id, game_id)
-VALUES (1, 'Iterative', 1, 'iterative.png', 1, 1),
-    (2, 'Incremental', 2, 'incremental.png', 1, 1),
-    (3, 'Bigbang', 3, 'bigbang.png', 1, 1);
+INSERT INTO tbl_answers (answer_content, number_order, answer_content_as_image, answer_group_id, game_id)
+VALUES ('Iterative', 1, 'iterative.png', 1, 1),
+    ('Incremental', 2, 'incremental.png', 1, 1),
+    ('Bigbang', 3, 'bigbang.png', 1, 1);
 
 INSERT INTO public.tbl_game_boards
-(id, code, game_id)
-VALUES(1, 'b4661d5e-f296-4cf6-887d-cfa0f97d1f36', 1),
-    (2, 'asd6gfga-f296-sdf3-0fn2-asf86gc1crt2', 1);
+(code, game_id)
+VALUES('b4661d5e-f296-4cf6-887d-cfa0f97d1f36', 1),
+    ('asd6gfga-f296-sdf3-0fn2-asf86gc1crt2', 1);
 
 INSERT INTO public.tbl_players
-(id, name, game_board_id)
-VALUES(1,'Strawberries', 1),
-    (2,'Banana', 1),
-    (3,'Orange', 1),
-    (4,'Mango', 1),
-    (5,'Durian', 1);
+(name, game_board_id)
+VALUES('Strawberries', 1),
+    ('Banana', 1),
+    ('Orange', 1),
+    ('Mango', 1),
+    ('Durian', 1);
 
 INSERT INTO public.tbl_answered_questions
-(id, game_board_id, question_content, playing)
-VALUES(1,1,'Building a new highway', true),
-    (2,1,'Learning to ride a horse', false),
-    (3,1,'Sewing a patchwork quilt', false),
-    (4,1,'Negotiating the release of a kidnapped person', false);
+(game_board_id, question_content, playing)
+VALUES(1, 'Building a new highway', true),
+    (1, 'Learning to ride a horse', false),
+    (1, 'Sewing a patchwork quilt', false),
+    (1, 'Negotiating the release of a kidnapped person', false);
 
 
 INSERT INTO public.tbl_questions
-(id, question_content, game_id)
-VALUES(1,'Building a new highway', 1),
-    (2,'Learning to ride a horse', 1),
-    (3,'Sewing a patchwork quilt', 1),
-    (4,'Negotiating the release of a kidnapped person', 1),
-    (5,'Learning to speak in public', 1),
-    (6,'Learning to dive', 1),
-    (7,'Building a tree house', 1);
+(question_content, game_id)
+VALUES('Building a new highway', 1),
+    ('Learning to ride a horse', 1),
+    ('Sewing a patchwork quilt', 1),
+    ('Negotiating the release of a kidnapped person', 1),
+    ('Learning to speak in public', 1),
+    ('Learning to dive', 1),
+    ('Building a tree house', 1);
 
 INSERT INTO public.tbl_answered_question_details
-(id, answered_question_id, player_id, answer_content_as_image)
-VALUES(1,1,1,'iterative.png'),
-    (2,1,2,'bigbang.png'),
-    (3,1,3,'incremental.png'),
-    (4,1,4,'iterative.png'),
-    (5,1,5, null);
+(answered_question_id, player_id, answer_content_as_image)
+VALUES(1,1,'iterative.png'),
+    (1,2,'bigbang.png'),
+    (1,3,'incremental.png'),
+    (1,4,'iterative.png'),
+    (1,5, null);
--- src/test/java/com/axonactive/agiletools/agiledeck/gameboard/boundary/GameBoardResourceTest.java
+++ src/test/java/com/axonactive/agiletools/agiledeck/gameboard/boundary/GameBoardResourceTest.java
@@ -13,13 +13,7 @@ import io.restassured.response.Response;
 
 @QuarkusTest
 class GameBoardResourceTest {
-    @Test
-    public void whenCreateNewGameBoard_thenReturnLocationInHeader() {
-        Response response = RestAssured.given().queryParam("game", 1).when().put("gameboards");
 
-        Assertions.assertEquals(201, response.getStatusCode());
-        Assertions.assertNotNull(response.getHeader("Location"));
-    }
 
     @Test
     public void whenCreateNewGameBoard_thenReturnGameNotFound() {
@@ -30,7 +24,7 @@ class GameBoardResourceTest {
     @Test
     public void whenJoinGame_thenReturnAnswerQuestionDetail() {
         Response response = RestAssured.given().pathParam("code", "b4661d5e-f296-4cf6-887d-cfa0f97d1f36").when()
-                .get("gameboards/{code}");
+                .get("gameboards/join/{code}");
 
         Assertions.assertEquals(200, response.getStatusCode());
     }
@@ -39,30 +33,16 @@ class GameBoardResourceTest {
     public void whenJoinGameBusAnswerQuestionNotFound_thenReturnDefaultAnswerQuestionDetail(){
         Response response = RestAssured.given().pathParam("code", "b4661d5e-f296-4cf6-887d-cfa0f97d1f36")
                             .when()
-                            .get("gameboards/{code}");
+                            .get("gameboards/join/{code}");
     }
 
     @Test
     public void whenJoinGame_thenReturnAnswer() {
-        RestAssured.given().pathParam("code", "code-not-found").when().get("gameboards/{code}").then().statusCode(400)
+        RestAssured.given().pathParam("code", "code-not-found").when().get("gameboards/join/{code}").then().statusCode(400)
                 .header("MSG_CODE", CoreMatchers.is("GAME_BOARD_NOT_FOUND"));
     }
 
-    @Test
-    public void whenPlayerChooseAnswer_thenReturnAnswerQuestionDetail() {
 
-        JsonObject answerContent = (JsonObject) Json.createObjectBuilder()
-                                .add("content", "Bigbang")
-                                .add("contentAsImage", "bigbang.png")
-                                .build();
-        
-        Response response = RestAssured.given().pathParam("answerQuestionDetailId", 5)
-                                .header("Content-Type", "application/json")
-                                .body(answerContent)
-                                .when().put("gameboards/{answerQuestionDetailId}");
-
-        Assertions.assertEquals(200, response.getStatusCode());
-    }
 
     @Test
     public void whenPlayerChooseAnswer_thenReturnNotFoundAnswerQuestionDetail(){
@@ -71,13 +51,39 @@ class GameBoardResourceTest {
                                 .add("content", "Bigbang")
                                 .add("contentAsImage", "bigbang.png")
                                 .build();
-        
+
         RestAssured.given().pathParam("answerQuestionDetailId", -1)
                 .header("Content-Type", "application/json")
                 .body(answerContent)
-                .when().put("gameboards/{answerQuestionDetailId}")
+                .when().put("answeredquestiondetails/{answerQuestionDetailId}")
                 .then()
                 .statusCode(400)
                 .header("MSG_CODE", CoreMatchers.is("ANSWER_QUESTION_DETAIL_NOT_FOUND"));
+    }
+
+
+    @Test
+    public void whenCreateNewGameBoard_thenReturnLocationInHeader() {
+        Response response = RestAssured.given().queryParam("game", 1).when().put("gameboards");
+
+        Assertions.assertEquals(201, response.getStatusCode());
+        Assertions.assertNotNull(response.getHeader("Location"));
+    }
+
+
+    @Test
+    public void whenPlayerChooseAnswer_thenReturnAnswerQuestionDetail() {
+
+        JsonObject answerContent = (JsonObject) Json.createObjectBuilder()
+                .add("content", "Bigbang")
+                .add("contentAsImage", "bigbang.png")
+                .build();
+
+        Response response = RestAssured.given().pathParam("answerQuestionDetailId", 5)
+                .header("Content-Type", "application/json")
+                .body(answerContent)
+                .when().put("answeredquestiondetails/{answerQuestionDetailId}");
+
+        Assertions.assertEquals(200, response.getStatusCode());
     }
 }
