f607e4bc8a73734dd1019a12fbaba5be09619792
LNHTai <tai.le01@axonactive.com>
2020-11-23 09:33:40 +0700

    Change player socket class

--- src/main/java/com/axonactive/agiletools/agiledeck/gameboard/entity/PlayerSocket.java
+++ src/main/java/com/axonactive/agiletools/agiledeck/gameboard/entity/PlayerSocket.java
@@ -1,14 +1,20 @@
 package com.axonactive.agiletools.agiledeck.gameboard.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PlayerSocket extends Player{
-    boolean selected = false;
-    Integer selectedCardId = -1;
+    private Long id;
+    private GameBoard gameBoard;
+    private String name;
+    private String avatar;
+    private Boolean selected = false;
+    private Integer selectedCardId = -1;
 
     @Override
     public boolean equals(Object o) {
