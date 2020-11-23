1ba724a1bef10a9f3a39655b8b4fef03a855ea16
Net Huynh Mi <hmnet@axonactive.com>
2020-11-23 09:38:17 +0700

    update

--- src/test/java/com/axonactive/agiletools/agiledeck/gameboard/boundary/GameBoardSocketTest.java
+++ src/test/java/com/axonactive/agiletools/agiledeck/gameboard/boundary/GameBoardSocketTest.java
@@ -4,11 +4,16 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.websocket.Session;
 
 import com.axonactive.agiletools.agiledeck.game.entity.Game;
 import com.axonactive.agiletools.agiledeck.gameboard.entity.GameBoard;
+import com.axonactive.agiletools.agiledeck.gameboard.entity.PlayerSocket;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 // import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -24,16 +29,24 @@ public class GameBoardSocketTest {
     @Mock
     GameBoardSocket gameBoardSocket;
 
-    Game game = new Game();
+    Game game;
+
+    GameBoard gameBoard;
+
+    @BeforeEach
+    public void init(){
+        game.setName("Agile Deck");
+        gameBoard = new GameBoard("b4661d5e-f296-4cf6-887d-cfa0f97d1f36", game);
+    }
 
     @Test
     public void whenOnOpenSocket(){
         Session session = Mockito.mock(Session.class);
-        game.setName("Agile Deck");
-        GameBoard gameBoard = new GameBoard("b4661d5e-f296-4cf6-887d-cfa0f97d1f36", game);
         gameBoardSocket = mock(GameBoardSocket.class);
-        gameBoardSocket.onOpen(session, gameBoard.getCode());
+        gameBoardSocket.onOpen(session, this.gameBoard.getCode());
+
         // when(this.gameBoardSocket).thenReturn(null);
+
         verify(gameBoardSocket).onOpen(Mockito.any(), Mockito.anyString());
         verify(gameBoardSocket, times(1));
 
@@ -49,7 +62,8 @@ public class GameBoardSocketTest {
 
     @Test
     public void whenFilterPlayer(){
-        // GameBoard gameBoard = new GameBoard("b4661d5e-f296-4cf6-887d-cfa0f97d1f36", game);
-        //List<PlayerSocket> player = new A
+        
+        List<PlayerSocket> player = new ArrayList<>();
+        
     }
 }
