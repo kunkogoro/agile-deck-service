0cd2e466bf60afca169392d585830fa819b67c83
LNHTai <tai.le01@axonactive.com>
2020-11-25 13:30:28 +0700

    Updated

--- src/main/java/com/axonactive/agiletools/agiledeck/gameboard/control/PlayerService.java
+++ src/main/java/com/axonactive/agiletools/agiledeck/gameboard/control/PlayerService.java
@@ -10,11 +10,11 @@ import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import com.axonactive.agiletools.agiledeck.AgileDeckException;
-// import com.axonactive.agiletools.agiledeck.Faker;
+import com.axonactive.agiletools.agiledeck.Faker;
 import com.axonactive.agiletools.agiledeck.gameboard.entity.GameBoard;
 import com.axonactive.agiletools.agiledeck.gameboard.entity.Player;
 import com.axonactive.agiletools.agiledeck.gameboard.entity.PlayerMsgCodes;
-import com.github.javafaker.Faker;
+//import com.github.javafaker.Faker;
 
 @RequestScoped
 @Transactional
