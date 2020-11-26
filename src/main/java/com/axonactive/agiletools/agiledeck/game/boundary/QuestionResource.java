ee40974d8d0a4935f45bc8b51eb31cf394bec8fd
Dat Pham Hieu <phdat@axonactive.com>
2020-11-26 11:40:13 +0700

    fix socket for disabled next-scenario when has no scenarios left

--- src/main/java/com/axonactive/agiletools/agiledeck/game/boundary/QuestionResource.java
+++ src/main/java/com/axonactive/agiletools/agiledeck/game/boundary/QuestionResource.java
@@ -1,6 +1,8 @@
 package com.axonactive.agiletools.agiledeck.game.boundary;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -12,6 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.axonactive.agiletools.agiledeck.AgileDeckException;
 import com.axonactive.agiletools.agiledeck.game.control.GameService;
 import com.axonactive.agiletools.agiledeck.game.control.QuestionService;
 import com.axonactive.agiletools.agiledeck.game.entity.Game;
@@ -42,22 +45,30 @@ public class QuestionResource {
     @GET
     @Path("/{code}")
     public Response getQuestion(@PathParam("code") String code){
-
+        Boolean isLastOne = false;
         GameBoard gameBoard = gameBoardService.getByCode(code);
         Game game = gameService.findById(gameBoard.getGame().getId());
 
         AnsweredQuestion currentAnsweredQuestion = answeredQuestionService.findCurrenrPLaying(gameBoard.getId());
         currentAnsweredQuestion.setPlaying(false);
         answeredQuestionService.updateStatusPlaying(currentAnsweredQuestion);
-
         
         List<Question> listQuestion = questionService.getAllByGameID(game.getId());
 
         Question question = questionService.random(listQuestion, gameBoard.getId());
-
         AnsweredQuestion newAnsweredQuestion =  answeredQuestionService.create(question, gameBoard);
+        try{
+            questionService.random(listQuestion, gameBoard.getId());
+        }catch(AgileDeckException ade) {
+            System.out.println(ade.getMessage());
+            isLastOne = true;
+        }
 
-        return Response.ok(newAnsweredQuestion).build();
+
+        Map<String, Object> data = new ConcurrentHashMap<>();
+        data.put("answeredQuestion", newAnsweredQuestion);
+        data.put("isLastOne", isLastOne);
+        return Response.ok(data).build();
     }
 
 }
