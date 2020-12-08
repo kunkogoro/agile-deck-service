b5118c399af327cdef8bf8a19261232dd43deaba
Minh Vo Hoang <vhminh@axonactive.com>
2020-12-08 08:54:32 +0700

    update question without test case

---
+++ src/main/java/com/axonactive/agiletools/agiledeck/gameboard/boundary/AnsweredQuestionResource.java
@@ -0,0 +1,35 @@
+package com.axonactive.agiletools.agiledeck.gameboard.boundary;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.axonactive.agiletools.agiledeck.gameboard.control.AnsweredQuestionService;
+import com.axonactive.agiletools.agiledeck.gameboard.entity.AnsweredQuestion;
+
+@Path("/answeredquestions")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
+@Transactional
+public class AnsweredQuestionResource {
+    
+    @Inject
+    AnsweredQuestionService answeredQuestionService;
+
+    @PUT
+    @Path("{id}")
+    public Response updateAnsweredQuestion(@PathParam("id") Long answeredQuestionId, AnsweredQuestion newAnsweredQuestion){
+        AnsweredQuestion answeredQuestion = answeredQuestionService.update(answeredQuestionId,newAnsweredQuestion);
+        
+        return Response.ok(answeredQuestion).build();
+    }
+}
--- src/main/java/com/axonactive/agiletools/agiledeck/gameboard/control/AnsweredQuestionService.java
+++ src/main/java/com/axonactive/agiletools/agiledeck/gameboard/control/AnsweredQuestionService.java
@@ -1,6 +1,7 @@
 package com.axonactive.agiletools.agiledeck.gameboard.control;
 
 import com.axonactive.agiletools.agiledeck.game.entity.Question;
+import com.axonactive.agiletools.agiledeck.game.entity.QuestionContent;
 import com.axonactive.agiletools.agiledeck.gameboard.entity.AnsweredQuestion;
 import com.axonactive.agiletools.agiledeck.gameboard.entity.GameBoard;
 
@@ -41,5 +42,14 @@ public class AnsweredQuestionService {
         TypedQuery<AnsweredQuestion> query = em.createNamedQuery(AnsweredQuestion.GET_BY_GAME_BOARD_ID, AnsweredQuestion.class);
         query.setParameter("id", id);
         return query.getResultList();
+    }
+
+    public AnsweredQuestion findById(Long id){
+        return em.find(AnsweredQuestion.class, id);
+    }
+    public AnsweredQuestion update(Long id, AnsweredQuestion updateAnsweredQuestion){
+        AnsweredQuestion answeredQuestion= findById(id);
+        answeredQuestion.setContent(updateAnsweredQuestion.getContent());
+        return em.merge(answeredQuestion);
     }
 }
